c3821a0c7843c5ddab05582a8386c97c9d6aaaa0
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-07-04 15:49:23 +0800

    Add KeyValuePane

---
+++ src/components/panes/KeyValuePane.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Tab } from 'semantic-ui-react';
+
+
+const KeyValuePane = () => {
+
+  return (
+    <Tab.Pane>
+      <AddKeyValue/>
+    </Tab.Pane>
+  )
+
+}
+
+export default KeyValuePane;
