dc9a9320d4ccd1d3a23f6891f93e9ff2b9929323
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-07-04 20:43:45 +0800

    Pass key values to update the parents state

---
+++ src/components/panes/KeyValuePane.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import AddKeyValue from './AddKeyValue';
+import { Tab, Button } from 'semantic-ui-react';
+
+class KeyValuePane extends React.Component {
+  
+  state = {
+    keyValues: [
+      {
+        id: 0,
+        keyItem : '',
+        valueItem : ''
+      }
+    ]
+  }
+
+  handleOnClick = () => {
+    this.setState({
+      ...this.state.keyValues.push({
+        id: this.state.keyValues.length,
+        keyItem: '',
+        valueItem: ''
+      })
+    })
+  }
+
+  onKeyPairUpdate = (keyPair) => {
+    // Reference:
+    // https://javascript.plainenglish.io/react-updating-a-value-in-state-array-7bae7c7eaef9
+
+    // first look for the elements index
+    const elementIndex = this.state.keyValues.findIndex(element => element.id === keyPair.id);
+
+    let newKeyValues = [...this.state.keyValues];
+
+    newKeyValues[elementIndex] = {
+      ...newKeyValues[elementIndex], 
+      keyItem: keyPair.keyItem,
+      valueItem: keyPair.valueItem
+    }
+
+    this.setState({
+      keyValues: newKeyValues
+    });
+  }
+
+  render(){
+
+    const renderedList = this.state.keyValues.map((keyPair, index) => {
+      return <AddKeyValue
+          key={index}
+          keyPair={keyPair} 
+          onKeyPairUpdate={this.onKeyPairUpdate} />
+      });
+
+    return (
+      <Tab.Pane>
+        {renderedList}
+        <Button onClick={(e) => this.handleOnClick(e)}>Add</Button>
+      </Tab.Pane>
+    )
+  }
+}
+
+export default KeyValuePane;
