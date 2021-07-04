569529285b4dec8608811b3f6cfae66ab020a697
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-07-04 20:43:14 +0800

    Allow set up add key value

--- src/components/panes/AddKeyValue.jsx
+++ src/components/panes/AddKeyValue.jsx
@@ -1,16 +1,60 @@
 import React from 'react';
-import { Input, Button } from 'semantic-ui-react';
+import { 
+  Input, 
+  Button,
+  Grid
+} from 'semantic-ui-react';
 
-const AddKeyValue = () => {
+class AddKeyValue extends React.Component {
   
-  return (
-    <div className="add-key-value">
-      <Input placeholder='Key'/>
-      <Input placeholder='Value'/>
-      <Button>Add</Button>
-    </div>
-  )
+  state = {
+    id: 0,
+    keyItem: '',
+    valueItem: ''
+  }
 
+  handleOnChange = (e) => {
+    this.setState({
+      id: this.props.keyPair.id,  
+      [e.target.name] : e.target.value
+    });
+
+    this.props.onKeyPairUpdate(this.state);
+  }
+
+  render(){
+    return (
+      <div className="add-key-value">
+        <Grid divided='vertically'>
+          <Grid.Row columns={3}>
+  
+            <Grid.Column width={7}>
+              <Input 
+                fluid 
+                placeholder='Key'
+                name='keyItem'
+                onChange={(e) => this.handleOnChange(e)}/>
+            </Grid.Column>
+  
+            <Grid.Column width={7}>
+              <Input 
+                fluid 
+                placeholder='Value'
+                name='valueItem'
+                onChange={(e) => this.handleOnChange(e)}/>
+            </Grid.Column>
+  
+            <Grid.Column width={2}>
+              <Button >Remove</Button>
+            </Grid.Column>
+  
+          </Grid.Row>
+        </Grid>
+      </div>
+    );
+  }
+ 
+  
 }
 
 export default AddKeyValue;
