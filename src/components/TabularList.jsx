2b17767786da9c61f89a6784af4fbf9bc7a68507
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-07-05 19:59:13 +0800

    move state for keyValuePanes into tabularList

--- src/components/TabularList.jsx
+++ src/components/TabularList.jsx
@@ -2,28 +2,116 @@ import React from 'react';
 import { Tab } from 'semantic-ui-react';
 import KeyValuePane from './panes/KeyValuePane';
 
-const panes = [
-  {
-    menuItem: 'Query Params',
-    render: () => <KeyValuePane/>
-  },
-  { 
-    menuItem: 'Headers', 
-    render: () => <KeyValuePane/>
-  },
-  { 
-    menuItem: 'JSON', 
-    render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> 
-  }
-]
+class TabularList extends React.Component {
 
-const TabularList = () => {
-  
-  return (
-    <div className="tabular-list">
-      <Tab panes={panes}/>
-    </div>
-  )
+  state = {
+    queryParams: [
+      {
+        id: 0,
+        keyItem : '',
+        valueItem : ''
+      }
+    ],
+    headers: [
+      {
+        id: 0,
+        keyItem : '',
+        valueItem : ''
+      }
+    ]
+  }
+
+  // configuration for panes
+  panes = [
+    {
+      menuItem: 'Query Params',
+      render: () => <KeyValuePane
+                      tabName='queryParams'
+                      keyValueList={this.state.queryParams}
+                      onKeyPairAdd={this.onKeyPairAdd}
+                      onKeyPairUpdate={this.onKeyPairUpdate}
+                      onKeyPairRemove={this.onKeyPairRemove}
+                    />
+    },
+    {
+      menuItem: 'Headers',
+      render: () => <KeyValuePane
+                      tabName='headers'
+                      keyValueList={this.state.headers}
+                      onKeyPairAdd={this.onKeyPairAdd}
+                      onKeyPairUpdate={this.onKeyPairUpdate}
+                      onKeyPairRemove={this.onKeyPairRemove}
+                    />
+    },
+    { 
+      menuItem: 'JSON', 
+      render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> 
+    }
+  ]
+
+  onKeyPairAdd = (tabName) => {
+    this.setState({
+      ...this.state[tabName].push({
+        id: this.state[tabName].length,
+        keyItem: '',
+        valueItem: ''
+      })
+    }, () => {
+      console.log(this.state);
+    })
+  }
+
+  onKeyPairRemove = (tabName, keyPair) => {
+    
+    let newKeyValues = [...this.state[tabName]];
+
+    // render a new list removing based on the index
+    newKeyValues = newKeyValues.filter(x => x.id !== keyPair.id);
+
+    this.setState({
+      [tabName] : newKeyValues
+    }, () => {
+      console.log(this.state)
+    });
+  }
+
+  onKeyPairUpdate = (tabName, keyPair) => {
+    // Reference:
+    // https://javascript.plainenglish.io/react-updating-a-value-in-state-array-7bae7c7eaef9
+
+    // first look for the elements index
+    const elementIndex = this.state[tabName].findIndex(element => element.id === keyPair.id);
+    
+    let newKeyValues = [...this.state[tabName]];
+
+    newKeyValues[elementIndex] = {
+      ...newKeyValues[elementIndex], 
+      keyItem: keyPair.keyItem,
+      valueItem: keyPair.valueItem
+    }
+
+    this.setState({
+      [tabName]: newKeyValues
+    }, () => {
+      console.log(this.state)
+    });
+  }
+
+  onTabChange = () => {
+    console.log('tab changed 1')
+  }
+
+  render(){
+    return (
+      <div className="tabular-list">
+        <Tab 
+          panes={this.panes}
+          onTabChange={this.onTabChange}
+        />
+      </div>
+    );
+  }
+
 }
 
 export default TabularList;
--- src/components/panes/KeyValuePane.jsx
+++ src/components/panes/KeyValuePane.jsx
@@ -4,76 +4,31 @@ import { Tab, Button } from 'semantic-ui-react';
 
 class KeyValuePane extends React.Component {
   
-  state = {
-    keyValues: [
-      {
-        id: 0,
-        keyItem : '',
-        valueItem : ''
-      }
-    ]
+  handleOnClick = (tabName) => {
+    this.props.onKeyPairAdd(tabName);
   }
 
-  handleOnClick = () => {
-    this.setState({
-      ...this.state.keyValues.push({
-        id: this.state.keyValues.length,
-        keyItem: '',
-        valueItem: ''
-      })
-    })
+  onKeyPairRemove = (tabName, keyPair) => {
+    this.props.onKeyPairRemove(tabName, keyPair);
   }
 
-  onKeyPairRemove = (keyPair) => {
-    
-    let newKeyValues = [...this.state.keyValues];
-
-    // render a new list removing based on the index
-    newKeyValues = newKeyValues.filter(x => x.id !== keyPair.id);
-
-    this.setState({
-      keyValues: newKeyValues
-    }, () => {
-      console.log(this.state)
-    });
-  }
-
-  onKeyPairUpdate = (keyPair) => {
-    // Reference:
-    // https://javascript.plainenglish.io/react-updating-a-value-in-state-array-7bae7c7eaef9
-
-    // first look for the elements index
-    const elementIndex = this.state.keyValues.findIndex(element => element.id === keyPair.id);
-
-    let newKeyValues = [...this.state.keyValues];
-
-    newKeyValues[elementIndex] = {
-      ...newKeyValues[elementIndex], 
-      keyItem: keyPair.keyItem,
-      valueItem: keyPair.valueItem
-    }
-
-    this.setState({
-      keyValues: newKeyValues
-    }, ()=> {
-      console.log(this.state)
-    });
+  onKeyPairUpdate = (tabName, keyPair) => {
+    this.props.onKeyPairUpdate(tabName, keyPair);
   }
 
   render(){
-
-    const renderedList = this.state.keyValues.map((keyPair) => {
+    const renderedList = this.props.keyValueList.map((keyPair) => {
       return <AddKeyValue
           key={keyPair.id}
-          keyPair={keyPair} 
-          onKeyPairUpdate={this.onKeyPairUpdate}
-          onKeyPairRemove={this.onKeyPairRemove} />
+          keyPair={keyPair}
+          onKeyPairUpdate={(kpValue) => this.onKeyPairUpdate(this.props.tabName, kpValue)}
+          onKeyPairRemove={() => this.onKeyPairRemove(this.props.tabName, keyPair)} />
       });
 
     return (
       <Tab.Pane>
         {renderedList}
-        <Button onClick={(e) => this.handleOnClick(e)}>Add</Button>
+        <Button onClick={() => this.handleOnClick(this.props.tabName)}>Add</Button>
       </Tab.Pane>
     )
   }
