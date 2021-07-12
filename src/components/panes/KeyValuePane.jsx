d32e7273d18c52a945586d08455058d1b9465ef3
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-07-12 20:53:57 +0800

    Move all state logic to KeyValuePane

--- src/components/panes/KeyValuePane.jsx
+++ src/components/panes/KeyValuePane.jsx
@@ -1,23 +1,66 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import AddKeyValue from './AddKeyValue';
-import { Tab, Button } from 'semantic-ui-react';
+import { Button } from 'semantic-ui-react';
+import { v4 as uuidv4 } from 'uuid';
 
-const KeyValuePane = ({ tabName, keyValueList, onKeyPairAdd, onKeyPairRemove, onKeyPairUpdate  }) => {
+const KeyValuePane = () => {
+
+  const keyPairInitState = [
+    {
+      id: uuidv4(),
+      keyItem : '',
+      valueItem : ''
+    }
+  ] 
+
+  const [keyPairs, setKeyPairs ] = useState(keyPairInitState);
+
+  const onKeyPairAdd = () => {
+    setKeyPairs(keyPairs => [...keyPairs, {
+      id: uuidv4(),
+      keyItem: '',
+      valueItem: ''
+    }])
+  }
+
+  const onKeyPairRemove = (keyPair) => {
+    let newKeyValues = [...keyPairs];
+    newKeyValues = newKeyValues.filter(x => x.id !== keyPair.id);
+    setKeyPairs(newKeyValues);
+  }
+
+  const onKeyPairUpdate = useCallback((keyPair) => {
+    // Reference:
+    // https://javascript.plainenglish.io/react-updating-a-value-in-state-array-7bae7c7eaef9
+
+    // first look for the elements index
+
+    const elementIndex = keyPairs.findIndex(element => element.id === keyPair.id);
+    let newKeyValues = [...keyPairs];
+    
+    newKeyValues[elementIndex] = {
+      ...newKeyValues[elementIndex], 
+      keyItem: keyPair.keyItem,
+      valueItem: keyPair.valueItem
+    };
+    
+    setKeyPairs(newKeyValues);
+  }, [keyPairs])
 
 
-  const renderedList = keyValueList.map((keyPair) => {
+  const renderedList = keyPairs.map((keyPair) => {
     return <AddKeyValue
         key={keyPair.id}
         keyPair={keyPair}
-        onKeyPairUpdate={(kpValue) => onKeyPairUpdate(tabName, kpValue)}
-        onKeyPairRemove={() => onKeyPairRemove(tabName, keyPair)} />
+        onKeyPairUpdate={(kpValue) => onKeyPairUpdate(kpValue)}
+        onKeyPairRemove={() => onKeyPairRemove(keyPair)} />
     });
 
   return (
-    <Tab.Pane>
+    <React.Fragment>
       {renderedList}
-      <Button onClick={() => onKeyPairAdd(tabName)}>Add</Button>
-    </Tab.Pane>
+      <Button onClick={() => onKeyPairAdd()}>Add</Button>
+    </React.Fragment>
   )
 
 }
