95ce49aaaac9c535819fccc1b2ab8fe321708c5c
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-07-13 13:23:01 +0800

    Add Response Panel and Response Tabs

---
+++ src/components/ResponsePanel.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Container, Grid } from 'semantic-ui-react';
+
+import ResponseTabs from './ResponseTabs';
+
+const styles = {
+  'padding': '2em'
+}
+
+const ResponsePanel = () => {
+  return (
+    <div style={styles}>
+      <Container fluid>
+        <h1>Response</h1>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column>
+              <div>Status: </div>
+            </Grid.Column>
+            <Grid.Column>
+              <div>Time: </div>
+            </Grid.Column>
+            <Grid.Column>
+              <div>Size: </div>
+            </Grid.Column>
+          </Grid.Row>
+
+          <Grid.Row columns={1}>
+            <Grid.Column width={16}>
+              <ResponseTabs/>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
+    </div>
+
+  );
+}
+
+export default ResponsePanel;
